4087fb92dc2787631909b23f2450c48367906d21
shshalam68 <a.k.mshahalam68@gmail.com>
2024-05-23 17:55:52 +0600

    Choose a New Password  Created

--- src/components/ResetPassword/ResetPasswordRequestForm.tsx
+++ src/components/ResetPassword/ResetPasswordRequestForm.tsx
@@ -1,34 +1,39 @@
+import { TextField } from "@mui/material";
 import React from "react";
 
 const ResetPasswordRequestForm = () => {
   return (
     <div className="min-h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-      <div className="bg-white rounded-lg px-8 py-6  max-w-lg w-full">
+      <div className="bg-white rounded-lg px-8 py-6 max-w-lg w-full">
         <h1 className="text-2xl font-semibold mb-4">Reset Password</h1>
-        <p className="mb-6">
-          Enter the email address you signed up with. We’ll send you an email
-          with a link to reset your password.
-        </p>
-        <form className="w-[342px]">
+        <div className="w-[463px] mb-6">
+          <p className="mb-4">
+            Enter the email address you signed up with. We’ll send you an email
+            with a link to reset your password.
+          </p>
+        </div>
+        <form className="w-[463px]">
           <div className="mb-4">
             <label htmlFor="email" className="block text-sm font-medium mb-2">
               Email
             </label>
-            <input
+            <TextField
               type="email"
               id="email"
-              className="shadow-sm rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-LawGuardPurple focus:border-LawGuardPurple"
+              className="shadow-sm rounded-md w-full  border border-gray-300 focus:outline-none focus:ring-LawGuardPurple focus:border-LawGuardPurple"
               required
             />
           </div>
           <button
             type="submit"
-            className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-LawGuardPrimary hover:bg-LawGuardPrimary focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-LawGuardPrimary"
+            className="w-full flex justify-center py-3 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-LawGuardPrimary hover:bg-LawGuardPrimary focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-LawGuardPrimary"
           >
             Send Reset Link
           </button>
-          <div className="text-center mt-2">
-            <button className="font-semibold text-xl " type="button">Cancel</button>
+          <div className="text-center mt-8">
+            <button className="text-xl font-semibold" type="button">
+              Cancel
+            </button>
           </div>
         </form>
       </div>
