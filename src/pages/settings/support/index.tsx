ab49d7a911a4fe06aaa7a04b6d08546aca6f2e76
ainayeem <ain.nayeem1999@gmail.com>
2024-05-27 17:31:15 +0600

    setting layout folder added

--- src/pages/settings/support/index.tsx
+++ src/pages/settings/support/index.tsx
@@ -1,18 +1,17 @@
 import ProtectedLayout from "@/components/layout/ProtectedLayout";
-import React from "react";
-import Settings from "../settings";
 import SettingsLayout from "@/components/layout/SettingsLayout";
+import React from "react";
 
-const Security = () => {
+const Support = () => {
   return (
     <ProtectedLayout>
       <div className="flex">
         <SettingsLayout>
-          <div className="flex ">hdfsdfsd</div>
+          <div className="flex border border-red-800">hdfsdfsd</div>
         </SettingsLayout>
       </div>
     </ProtectedLayout>
   );
 };
 
-export default Security;
+export default Support;
