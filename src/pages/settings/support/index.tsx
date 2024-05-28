0524ceea55d206b0f1dc3c50bda7b17288acb90b
shshalam68 <a.k.mshahalam68@gmail.com>
2024-05-28 15:57:38 +0600

    Support created

--- src/pages/settings/support/index.tsx
+++ src/pages/settings/support/index.tsx
@@ -1,13 +1,14 @@
 import ProtectedLayout from "@/components/layout/ProtectedLayout";
 import SettingsLayout from "@/components/layout/SettingsLayout";
+import SupportComponent from "@/components/Settings/SupportComponent";
 import React from "react";
 
 const Support = () => {
   return (
     <ProtectedLayout>
-      <div className="flex">
+      <div className="w-full ">
         <SettingsLayout>
-          <div className="flex border border-red-800">hdfsdfsd</div>
+          <div className=" w-full"><SupportComponent/></div>
         </SettingsLayout>
       </div>
     </ProtectedLayout>
