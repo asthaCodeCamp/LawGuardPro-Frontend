89468c2a1d6dd072e82402f1c7db876c782cacf5
shshalam68 <a.k.mshahalam68@gmail.com>
2024-06-22 01:16:19 +0600

    KAN-9:  Case Table And Pagination

--- src/components/Case/CasePagination.tsx
+++ src/components/Case/CasePagination.tsx
@@ -5,17 +5,20 @@ import Stack from "@mui/material/Stack";
 import { useState } from "react";
 import { Theme } from "@emotion/react";
 
-export default function CasePagination() {
-  const [page, setPage] = useState(1);
+export default function CasePagination({pages,setPage,casesData}:any ) {
+  const {totalCount} = casesData;
+
+  let totalPage = Math.ceil(totalCount/5);
+  // const [page, setPage] = useState(1);
   const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
 
     setPage(value);
-    console.log(value);
+    console.log("page value",value);
   };
 
   return (
     <Stack spacing={2}>
-      <Pagination count={3} page={page} onChange={handleChange}  sx={{
+      <Pagination count={totalPage} page={pages} onChange={handleChange}  sx={{
           '& .MuiPaginationItem-page.Mui-selected': {
             backgroundColor: '#AC1CFF',
             color: '#fff',
