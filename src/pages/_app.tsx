698e9d646cdca92c04747ff34a71fcbe66d4ba5f
ainayeem <ain.nayeem1999@gmail.com>
2024-06-25 20:40:33 +0600

    toast css add in app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 import { useState } from "react";
 import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export default function App({ Component, pageProps }: AppProps) {
